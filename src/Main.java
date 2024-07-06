c9d574e938ea92d3d8bbfd933a8e3a0081736a15
MIFos <152510445+gitIliyaM@users.noreply.github.com>
2024-07-06 10:22:21 +0300

    xogame

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // класс class FieldX0 описывает всю игру, нужно использовать методы класса для начала игры
+        // void f() - создает экземпляр класса X0Game, метод play() запускает игру
+        // для начала игры выбрать какой класс использовать
+    }
+    void f(){
+        X0Game xo = new X0Game();
+        xo.play();
+    }
+}
